55de4372e18cfd22935dd8cbe41b819f8fd1473a
Stefano Casasola <ste.casasola@gmail.com>
2024-11-27 12:11:46 +0100

    Completes form's logic

--- src/components/Reservation.jsx
+++ src/components/Reservation.jsx
@@ -11,25 +11,90 @@ import { Form, Button } from 'react-bootstrap'
 // Ogni volta che un componente React deve interagire con un form,
 // questo componente deve possedere uno STATE
 
+// two-way data binding tra lo stato del componente e i campi del form
+
 class Reservation extends Component {
-  state = {}
+  // lo scopo di oggi è capire come lavorare in React con i FORM
+  // se prima (in vanilla JS) raccoglievate i valori dei campi del form
+  // solamente alla fine della compilazione dello stesso, quando intervenivate
+  // all'evento di submit, adesso con React il componente sarà SEMPRE in pieno
+  // controllo del contenuto del form, man mano che l'utente lo compilerà.
+  // Arrivati alla fine del form, il componente avrà già raccolto tutti i dati
+  // e dovrà semplicemente inviarli con la chiamata POST.
+  // Raggiungeremo questo obiettivo creando un collegamento a DUE-VIE (two-way data binding) su OGNI campo del form, collegandolo alla corrispondente proprietà di "reservation" nello state.
+
+  state = {
+    reservation: {
+      name: '',
+      phone: '',
+      numberOfPeople: '1',
+      smoking: false,
+      dateTime: '',
+      specialRequests: '',
+    },
+  }
 
   render() {
     return (
       <Form className="text-start">
         <Form.Group className="mb-3">
           <Form.Label>Nome prenotazione</Form.Label>
-          <Form.Control type="text" placeholder="Mario Rossi" required />
+          <Form.Control
+            type="text"
+            placeholder="Mario Rossi"
+            required
+            // PRIMA FRECCIA (LEGGO)
+            value={this.state.reservation.name}
+            // SECONDA FRECCIA (SCRIVO)
+            onChange={
+              // qui devo fare una funzione che in base a quello che ho scritto
+              // setti il valore di this.state.reservation.name
+              (e) => {
+                console.log('THIS', this)
+                console.log('e', e)
+                this.setState({
+                  reservation: {
+                    ...this.state.reservation, // phone, numberOfPeople etc.
+                    name: e.target.value, // contenuto attuale dell'input
+                  },
+                })
+              }
+            }
+          />
         </Form.Group>
 
         <Form.Group className="mb-3">
           <Form.Label>Numero di telefono</Form.Label>
-          <Form.Control type="tel" placeholder="340xxxxxxx" required />
+          <Form.Control
+            type="tel"
+            placeholder="340xxxxxxx"
+            required
+            value={this.state.reservation.phone}
+            onChange={(e) => {
+              this.setState({
+                reservation: {
+                  ...this.state.reservation,
+                  phone: e.target.value,
+                },
+              })
+            }}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3">
           <Form.Label>Quanti sarete?</Form.Label>
-          <Form.Select aria-label="Numero di persone">
+          <Form.Select
+            aria-label="Numero di persone"
+            value={this.state.reservation.numberOfPeople}
+            onChange={(e) => {
+              this.setState({
+                reservation: {
+                  ...this.state.reservation,
+                  numberOfPeople: e.target.value,
+                },
+              })
+            }}
+          >
             <option>1</option>
             <option>2</option>
             <option>3</option>
@@ -42,7 +107,19 @@ class Reservation extends Component {
         </Form.Group>
 
         <Form.Group className="mb-3">
-          <Form.Check type="checkbox" label="Tavolo all'esterno?" />
+          <Form.Check
+            type="checkbox"
+            label="Tavolo all'esterno?"
+            checked={this.state.reservation.smoking}
+            onChange={(e) => {
+              this.setState({
+                reservation: {
+                  ...this.state.reservation,
+                  smoking: e.target.checked,
+                },
+              })
+            }}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3">
@@ -50,6 +127,15 @@ class Reservation extends Component {
           <Form.Control
             type="datetime-local"
             required
+            value={this.state.reservation.dateTime}
+            onChange={(e) =>
+              this.setState({
+                reservation: {
+                  ...this.state.reservation,
+                  dateTime: e.target.value,
+                },
+              })
+            }
             min={`${new Date().getFullYear()}-${
               new Date().getMonth() + 1
             }-${new Date().getDate()}T00:00`}
@@ -58,7 +144,19 @@ class Reservation extends Component {
 
         <Form.Group className="mb-3">
           <Form.Label>Allergie, richieste particolari?</Form.Label>
-          <Form.Control as="textarea" rows={5} />
+          <Form.Control
+            as="textarea"
+            rows={5}
+            value={this.state.reservation.specialRequests}
+            onChange={(e) => {
+              this.setState({
+                reservation: {
+                  ...this.state.reservation,
+                  specialRequests: e.target.value,
+                },
+              })
+            }}
+          />
         </Form.Group>
 
         <Button variant="success" type="submit">
